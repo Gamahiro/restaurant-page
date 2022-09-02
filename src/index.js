100bd2ebf609ceb8af472209c50b45c9cc3cd0a4
Johan <jpettersen50@gmail.com>
2022-09-02 09:36:01 +0200

    add populate dom with js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 console.log('hello');
+
+const content = document.querySelector('#content');
+const element = document.createElement('div');
+
+function landingPage() {
+
+    element.innerHTML = `<img class="headerImg" src="../pr-food.jpg" alt="puerto rican food">
+    <div class="title">Welcome to an hour of escape</div>
+    <div class="aboutResto">At PR foods we strive to create an experience that will give our guests an escape to a Puerto Rican food experience</div>`;
+    content.appendChild(element);
+}
+
+landingPage();
