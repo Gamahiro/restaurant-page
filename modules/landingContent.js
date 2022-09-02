2e5f182fff8d4da2111682cec7a92676f6d7b954
Johan <jpettersen50@gmail.com>
2022-09-02 10:07:00 +0200

    modules for diff pages

---
+++ modules/landingContent.js
@@ -0,0 +1,15 @@
+
+const content = document.querySelector('#content');
+const element = document.createElement('div');
+
+function landingContent() {
+
+    element.innerHTML = `<img class="headerImg" src="../pr-food.jpg" alt="puerto rican food">
+    <div class="title">Welcome to an hour of escape</div>
+    <div class="aboutResto">At PR foods we strive to create an experience that will give our guests an escape to a Puerto Rican food experience</div>`;;
+    content.appendChild(element);
+}
+
+export {
+    landingContent,
+}
---
+++ modules/menuContent.js
@@ -0,0 +1,4 @@
+
+function menuContent() {
+    return ''
+}
