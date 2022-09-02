182caf3395b423284dc8370639d988a4181d916d
Johan <jpettersen50@gmail.com>
2022-09-02 13:28:13 +0200

    comment

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { aboutContent } from "../modules/aboutContent";
 import { menuContent } from "../modules/menuContent";
 import './style.css';
 
+// push = subtree git subtree push --prefix dist origin gh-pages
 
 landingContent();
 
